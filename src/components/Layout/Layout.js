b6753f40c7a6f70fca817c0a52b8cb35ea4a7152
Gonzalo Ramiro Alvarez Campos <gonzalo.alvarez@globant.com>
2018-09-28 11:30:15 -0300

    1.6v added toolbar and logo, minor css changes to margin and height

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Auxiliary from '../../hoc/Auxiliary';
 import classes from './Layout.css';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
 
 const layout = ( props  ) => {
 	return (
 		<Auxiliary>
 			<div>
-				Toolbar, SideDrawer, Backdrop
+				<Toolbar />
 			</div>
 			<main className = {classes.Content}>
 				{props.children}
---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const logo = () => {
+	return (
+		<div>
+			<img src = { require( '../../assets/Img/burger-logo.png' )} alt = 'logo' width = '50' heigth = '50' />
+		</div>
+	);
+}
+
+export default logo;
